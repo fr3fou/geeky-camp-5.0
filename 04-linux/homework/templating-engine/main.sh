e8f50ce514abf0cea3646637844d9da877ffd9e0
Simo Aleksandrov <simo3003@me.com>
2018-09-05 23:09:04 +0300

    Add test.sol

--- 04-linux/homework/templating-engine/main.sh
+++ 04-linux/homework/templating-engine/main.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 
-echo "I'm in $(pwd)"
-echo "I'm in $PWD"
---
+++ 06-blockchain/homework/Test.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.21;
+contract Test {
+    uint private counter = 0;
+    address public owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function inc() public {
+        require(msg.sender == owner && counter + 1 > counter);
+        counter++;
+    }
+    
+    function getCounter() public view returns (uint _counter)  {
+        _counter = counter;
+    }
+    
+}

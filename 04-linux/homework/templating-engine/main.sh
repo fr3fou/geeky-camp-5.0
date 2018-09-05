c755f2da75fe606746245fb7bb08980b6c3c2d4b
Simo Aleksandrov <simo3003@me.com>
2018-09-05 18:03:39 +0300

    Add templating-engine.sh

---
+++ 04-linux/homework/templating-engine/main.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "I'm in $(pwd)"
+echo "I'm in $PWD"
--- 04-linux/homework/test.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-if touch $1
-then
-	echo "uwu~ $1 was touched"
-else
-	echo "despacito, couldn't touch ;w;"
-fi
